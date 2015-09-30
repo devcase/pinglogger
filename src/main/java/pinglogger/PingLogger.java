e1d400ca1921dc728510dad19c7b706a051a5eeb
Thiago Hirata <hirata@devcase.com.br>
2015-09-30 13:57:59 -0300

    Ping Logger

---
+++ src/main/java/pinglogger/PingLogger.java
@@ -0,0 +1,76 @@
+package pinglogger;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PingLogger {
+
+	public static void main(String[] args) {
+		if(args == null || args.length < 2){
+			System.out.println("Modo de execução: java -jar pinglogger.jar 192.168.1.104 c:\\\\cmd\\\\logger.exe 192.168.1.104");
+			System.exit(0);
+		}
+		try {
+			char dirSeparator = args[1].contains("\\") ? '\\' : '/';
+			
+			String ipAddress = args[0];
+			String loggerCmd = args[1];
+			String loggerDir = args[1].substring(0, args[1].lastIndexOf(dirSeparator));
+			
+			
+			System.out.println("Executável logger: " + loggerCmd);
+			System.out.println("Diretório logger: " + loggerDir);
+			System.out.println("Endereço IP: " + ipAddress);
+
+			File fileLoggerDir = new File(loggerDir);
+			if(!fileLoggerDir.exists() || !fileLoggerDir.isDirectory()) {
+				throw new IllegalArgumentException("Diretório " + fileLoggerDir + " não encontrado");
+			}
+			File loggerExe = new File(loggerCmd);
+			if(!fileLoggerDir.exists()) {
+				throw new IllegalArgumentException("Executável " + loggerExe + " não encontrado");
+			}
+
+			
+			ProcessBuilder pingProcessBuilder = new ProcessBuilder("ping", "-n", "2", ipAddress);
+			ProcessBuilder loggerProcessBuilder = new ProcessBuilder(loggerCmd);
+			loggerProcessBuilder.directory(fileLoggerDir);
+			DateFormat df = new SimpleDateFormat("dd/MM/yyyy - HH:mm"); 
+			
+			while(true) {
+				Process loggerProc = null;
+				Process pingProc = null;
+				try {
+					//Executa ping
+					System.out.print(df.format(new Date()));
+					System.out.println(" - Executando ping no IP " + ipAddress);
+				    pingProc = pingProcessBuilder.start();
+				    
+					//aguarda 10 segundos 
+					Thread.sleep(10 * 1000);
+					
+					//Executa logger
+					System.out.print(df.format(new Date()));
+					System.out.println(" - Executando logger");
+					loggerProc = loggerProcessBuilder.start();
+					
+					//aguarda 15 minutos
+					Thread.sleep(15 * 60 * 1000 - 10 * 1000);
+				} finally {
+					if(pingProc != null) {
+						try { pingProc.destroy(); } catch (Exception ex) { ex.printStackTrace(); }
+					}
+					if(loggerProc != null) {
+						try { loggerProc.destroy(); } catch (Exception ex) { ex.printStackTrace(); }
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+}
